d392f1a24fa27478e90f47a7ae70823c99af7ccd
julia741 <73189357+julia741@users.noreply.github.com>
2021-06-12 13:10:48 +0200

    .

--- Projekt-Notatki/Models/Notatka.cs
+++ Projekt-Notatki/Models/Notatka.cs
@@ -1,5 +1,10 @@
 ﻿
-
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
 namespace Projekt_Notatki.Models
 {
     public class notatka
